09c6f204bce699e42bd1da5383f78b960aca9caf
Eustache Le Bihan <eulebihan@gmail.com>
2025-07-28 14:14:05 +0200

    remove fastconformer

--- src/transformers/models/__init__.py
+++ src/transformers/models/__init__.py
@@ -235,7 +235,7 @@ if TYPE_CHECKING:
     from .owlv2 import *
     from .owlvit import *
     from .paligemma import *
-    from .parakeet_ctc import *
+    from .parakeet import *
     from .patchtsmixer import *
     from .patchtst import *
     from .pegasus import *
